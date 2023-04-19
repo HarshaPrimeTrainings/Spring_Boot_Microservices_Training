8aeb4ccf11ae578b1df9ec5335723b7626f53b81
HarshaVardhan <athaluri.harsha@gmail.com>
2023-04-19 22:58:02 +0530

    RestTemplTRE

---
+++ RestTemplateDemo/OrderService/src/test/java/com/training/orderserivice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.orderserivice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
