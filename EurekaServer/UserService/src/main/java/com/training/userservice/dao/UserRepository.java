107a44b9fa117f44a4a994dacdbe74765c2e6c46
HarshaVardhan <athaluri.harsha@gmail.com>
2023-04-24 23:01:19 +0530

    EurekaServer

---
+++ EurekaServer/UserService/src/main/java/com/training/userservice/dao/UserRepository.java
@@ -0,0 +1,24 @@
+package com.training.userservice.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.training.userservice.controllers.model.User;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Integer>{
+	public User findByuname(String uname);
+	
+	public List<User> getByaddress(String aaddr);
+	
+	
+	@Query(value  =  "select * from user where address = :address",nativeQuery = true)
+	public User getNYUser(@Param(value = "address") String address);
+
+}
