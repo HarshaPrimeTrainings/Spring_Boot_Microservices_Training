c667967f562b41d3e98c9e46d378dc41f942777f
HarshaVardhan <athaluri.harsha@gmail.com>
2023-04-26 23:15:25 +0530

    sleuth&zipkin demo

---
+++ sleuth&zipkin/UserService/src/main/java/com/training/userservice/dto/OrderDto.java
@@ -0,0 +1,29 @@
+package com.training.userservice.dto;
+
+public class OrderDto {
+
+	private Integer  orderId;
+	private String orderName;
+	private String orderStatus;
+	
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public String getOrderName() {
+		return orderName;
+	}
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	
+	
+}
