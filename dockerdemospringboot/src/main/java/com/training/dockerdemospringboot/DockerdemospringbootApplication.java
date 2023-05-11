3d9853dcd5d22983e3f2a9e80fff61b60a4c2157
HarshaVardhan <athaluri.harsha@gmail.com>
2023-05-11 08:01:42 +0530

    dockerdemo

---
+++ dockerdemospringboot/src/main/java/com/training/dockerdemospringboot/DockerdemospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.training.dockerdemospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerdemospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerdemospringbootApplication.class, args);
+	}
+
+}
---
+++ dockerdemospringboot/src/test/java/com/training/dockerdemospringboot/DockerdemospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.dockerdemospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerdemospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
